47d868fe91a34a09aacc1e738fcc5a749fcad325
Mark-Kohrman <mark.kohrman@gmail.com>
2020-08-16 12:05:03 -0500

    adding dynamic note selector function

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="note-selectors">
-  <note-selector />
+  <note-selector 
+    v-for="note in notes"
+    v-bind:key="note.id"
+    v-bind:note="note"
+  />
     
   </div>
 </template>
@@ -11,6 +15,16 @@ export default {
   name: "note-selectors",
   components: {
     NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "Test numero uno", timestamp: Date.now() },
+        { id: 2, body: "Test numero dos", timestamp: Date.now() },
+        { id: 3, body: "Test numero tres", timestamp: Date.now() },
+        { id: 4, body: "Test numero cuatro", timestamp: Date.now() },
+      ],
+    };
   },
 };
 </script>
