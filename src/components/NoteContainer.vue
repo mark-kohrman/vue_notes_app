015e732e1eaac1e79fcbbde6b080184d1ea25884
Mark-Kohrman <mark.kohrman@gmail.com>
2020-08-16 11:17:36 -0500

    adding NoteEditor.vue component

--- src/components/NoteContainer.vue
+++ src/components/NoteContainer.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="note-container">
     <note-selectors />
-    
-    <div class="note-editor">
-      <p class="note-editor-info">Timestamp here...</p>
-      <textarea class="note-editor-input">
-        First note...
-        
-        Note text here...
-      </textarea>
-    </div>
+    <note-editor />
+
+
   </div>
 
 </template>
 <script>
 import NoteSelectors from "./NoteSelectors.vue";
+import NoteEditor from "./NoteEditor.vue";
 export default {
   name: "note-container",
   components: {
     NoteSelectors,
+    NoteEditor,
   },
 };
 </script>
--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="note-selectors">
   <note-selector />
-   
-
     
   </div>
 </template>
